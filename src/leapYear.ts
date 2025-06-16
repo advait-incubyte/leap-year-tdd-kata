8a771c5cf6444a6793d428b0978ea441c5fda506
Advait Maindalkar <advait@incubyte.co>
2025-06-16 18:53:40 +0530

    broke up compound condition for cleaner code

--- src/leapYear.ts
+++ src/leapYear.ts
@@ -1,12 +1,8 @@
 export const isLeapYear = (year: number) : boolean => {
-    if (year % 4000 === 0)
-        return false
-
-    if (year % 400 === 0)
-        return true
-
-    if (year % 100 !== 0 && year % 4 === 0)
-        return true
+    if (year % 4000 === 0) return false
+    if (year % 400 === 0) return true
+    if (year % 100 === 0) return false
+    if (year % 4 === 0) return true
 
     return false
 }
