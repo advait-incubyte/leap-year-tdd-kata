f850f9e965c4c6be6948bb720fddc8d8eeb54c11
Advait Maindalkar <advait@incubyte.co>
2025-06-16 18:44:55 +0530

    added guard condition to check for divisibility by 4000 and return false if so + split existing if condition for more readability

--- src/leapYear.ts
+++ src/leapYear.ts
@@ -1,5 +1,11 @@
 export const isLeapYear = (year: number) : boolean => {
-    if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0))
+    if (year % 4000 === 0)
+        return false
+
+    if (year % 400 === 0)
+        return true
+
+    if (year % 100 !== 0 && year % 4 === 0)
         return true
 
     return false
