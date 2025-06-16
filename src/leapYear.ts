d43b823703ec14d00984733f4f4f9e5f8f0d1598
Advait Maindalkar <advait@incubyte.co>
2025-06-16 18:09:04 +0530

    wrote code to pass test case

--- src/leapYear.ts
+++ src/leapYear.ts
@@ -2,5 +2,11 @@ export const isLeapYear = (year: number) : boolean => {
     if (year % 400 === 0)
         return true
 
+    if (year % 100 === 0 && year % 400 !== 0)
+        return false
+
+    if (year % 4 === 0)
+        return true
+
     return false
 }
