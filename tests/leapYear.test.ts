34848e697965d8938221112f56c598426fe54e91
Advait Maindalkar <advait@incubyte.co>
2025-06-16 16:13:47 +0530

    defined isLeapYear blueprint

--- tests/leapYear.test.ts
+++ tests/leapYear.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from 'vitest'
-
+import { isLeapYear } from '../src/leapYear'
+ 
 describe('Leap Year', () => {
     it('should be defined', () => {
         expect(isLeapYear).toBeDefined()
